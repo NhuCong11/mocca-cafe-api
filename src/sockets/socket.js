95180ac4b2dd38280f6e4a304393c25d9dd4e516
Công <yeusangtao96@gmail.com>
2025-04-17 16:44:19 +0700

    Update author information and modify URLs in email verification templates to reflect new domain

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['*', 'http://localhost:3000', 'https://nhucong03.id.vn'],
+    origin: ['*', 'http://localhost:3000', 'https://mocca.io.vn/vi'],
     methods: ['GET', 'POST'],
   },
 });
